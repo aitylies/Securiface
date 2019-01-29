7d1e3fb8de6f9813b6e0cba54f4a6ce11491a233
Dylan Chesnouard <dylan.chesnouard@protonmail.com>
2019-01-29 23:28:53 +0100

    [29/01/19 23:27 Dylan] Première page "ConnectFrame.java"

--- src/main/java/fr/securiface/Securiface/ConnectFrame.java
+++ src/main/java/fr/securiface/Securiface/ConnectFrame.java
@@ -2,18 +2,30 @@ package fr.securiface.Securiface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.InputStream;
+
 
 public class ConnectFrame extends JFrame {
+
+    public static final String APP_TITLE = "Securiface";
+
     public ConnectFrame () {
-        this.setTitle("Securiface");
+        // GLOBAL SETTINGS
+        this.setTitle(APP_TITLE);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+        // MAIN PANEL
         JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
         mainPanel.setBackground(Color.WHITE);
 
-        // Connect button
+        // GO SECURI LOGO
+        InputStream imgGoSecuriLogo = getClass().getResourceAsStream("");
+
+        // CONNECT BUTTON
         JButton btConnect = new JButton("CONNEXION");
         btConnect.setFont(new Font("Roboto", Font.PLAIN, 25));
         //btConnect.setBorderPainted(false);
@@ -21,14 +33,23 @@ public class ConnectFrame extends JFrame {
         //btConnect.setContentAreaFilled(false);
         btConnect.setForeground(Color.decode("#000000"));
         btConnect.setBackground(Color.decode("#379EC1"));
+        btConnect.setPreferredSize(new Dimension(50, 20));
 
-        // Securiface streamer
-        // ...
+        // SECURIFACE STREAMER
+        JPanel securifaceStreamerPanel = new JPanel();
+        securifaceStreamerPanel.setBackground(Color.decode("#379EC1"));
+        JLabel lblSecurifaceStreamer = new JLabel("Securiface");
+        lblSecurifaceStreamer.setForeground(Color.decode("#000000"));
+        securifaceStreamerPanel.add(lblSecurifaceStreamer);
 
-        mainPanel.add(btConnect);
+        // ADD COMPONENT TO MAIN PANEL
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.CENTER);
+        mainPanel.add(btConnect, BorderLayout.CENTER);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.PAGE_END);
+        mainPanel.add(securifaceStreamerPanel, BorderLayout.PAGE_END);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 30)), BorderLayout.PAGE_END);
         this.add(mainPanel);
-        this.pack();
-
+        //this.pack();
         this.setVisible(true);
     }
 }
