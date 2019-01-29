aa4757fa714a71ac8852b4e5b42ff630d21ed4ba
Dylan Chesnouard <dylan.chesnouard@protonmail.com>
2019-01-29 16:13:17 +0100

    [29/01/19 16:12 Dylan] Suppression des utilisations de webcam-capture

--- src/main/java/fr/securiface/Securiface/LoginFrame.java
+++ src/main/java/fr/securiface/Securiface/LoginFrame.java
@@ -1,7 +1,5 @@
 package fr.securiface.Securiface;
 
-import com.github.sarxos.webcam.*;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -15,43 +13,9 @@ public class LoginFrame extends JFrame {
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(Color.WHITE);
 
-        // Connect button
-        JButton btConnect = new JButton("CONNEXION");
-        btConnect.setFont(new Font("Roboto", Font.PLAIN, 25));
-        //btConnect.setBorderPainted(false);
-        //btConnect.setFocusPainted(false);
-        //btConnect.setContentAreaFilled(false);
-        btConnect.setForeground(Color.decode("#000000"));
-        btConnect.setBackground(Color.decode("#379EC1"));
-
-        // Securiface streamer
-        // ...
-
-        //WebcamPanel webcamPanel = GetWebcamPanel();
-        //JPanel webcamPanel = new JPanel();
-
-        //this.add(webcamPanel);
-        mainPanel.add(btConnect);
         this.add(mainPanel);
         this.pack();
 
         this.setVisible(true);
-    }
-
-    private WebcamPanel GetWebcamPanel(){
-        Webcam webcam = Webcam.getDefault();
-        if (webcam != null) {
-            System.out.println("Webcam : " + webcam.getName());
-        } else {
-            System.out.println("No webcam detected");
-        }
-        webcam.setViewSize(WebcamResolution.VGA.getSize());
-        WebcamPanel webcamPanel = new WebcamPanel(webcam);
-        webcamPanel.setFPSDisplayed(true);
-        webcamPanel.setDisplayDebugInfo(true);
-        webcamPanel.setImageSizeDisplayed(true);
-        webcamPanel.setMirrored(true);
-
-        return webcamPanel;
     }
 }
