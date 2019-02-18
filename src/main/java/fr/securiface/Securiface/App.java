a17dbd244f3c3855e662d4de8dc65f08f39d3460
Minitosh <marc.deshayes44@gmail.com>
2019-02-18 10:01:04 +0100

    Ajout / Suppression et Listing de Matériels OK

--- src/main/java/fr/securiface/Securiface/App.java
+++ src/main/java/fr/securiface/Securiface/App.java
@@ -1,18 +1,35 @@
 package fr.securiface.Securiface;
 
+import sun.misc.resources.Messages_zh_CN;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        Agents.getListAgents();
-        Agents.putAgent("Wick","John","55882");
+        Date maintenant = Date.valueOf(LocalDate.now());
+        LocalDate date_ajout = LocalDate.of(2019, Month.JANUARY, 18);
+
+        /*Agents.putAgent("Wick","John","55882");
         Agents.getListAgents();
         Agents.removeAgent("Wick");
-        Agents.getListAgents();
+        Agents.getListAgents();*/
+
+
+        /*Materiel.putMateriel("Grapin",date_ajout,maintenant,"Grapin de sécurité 45mm");
+        Materiel.getListMateriel();
+        Materiel.removeMateriel("Grapin");
+        Materiel.getListMateriel();*/
 
         ConnectFrame connectFrame = new ConnectFrame();
     }
--- src/main/java/fr/securiface/Securiface/Materiel.java
+++ src/main/java/fr/securiface/Securiface/Materiel.java
@@ -1,39 +1,43 @@
 package fr.securiface.Securiface;
 
 import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDate;
 
 /**
  * Classe permettant la connexion à la base de donnée
- * Et la manipulation des différents agents.
+ * Et la manipulation des différents matériels
  */
 
-public class Agents {
+public class Materiel {
 
-    public static void getListAgents() {
+    public static void getListMateriel() {
 
         try {
-           Connection connection = ConnectBDD.getConnection();
+            Connection connection = ConnectBDD.getConnection();
             /* Création de l'objet gérant les requêtes */
             Statement statement = connection.createStatement();
 
             /* Exécution d'une requête de lecture */
-            ResultSet resultat = statement.executeQuery( "SELECT matricule, nom, prenom FROM agents;" );
+            ResultSet resultat = statement.executeQuery( "SELECT nom, date_ajout, dern_retrait, description FROM materiel;" );
 
             /* Récupération des données du résultat de la requête de lecture */
             while ( resultat.next() ) {
-                String nomAgent = resultat.getString( "nom" );
-                String prenomAgent = resultat.getString( "prenom" );
-                String matriculeAgent = resultat.getString( "matricule" );
+                String nomMateriel = resultat.getString( "nom" );
+                String date_ajoutMateriel = resultat.getString( "date_ajout" );
+                String dern_retraitMateriel = resultat.getString( "dern_retrait" );
+                String description = resultat.getString("description");
 
                 /* Traiter ici les valeurs récupérées. */
-                System.out.println("Agent : "+prenomAgent+" "+nomAgent+" , matricule : "+matriculeAgent);
+                System.out.println("\nMatériel : "+nomMateriel+"\nDate d'ajout : "+date_ajoutMateriel+"\nDernier Retrait : "+dern_retraitMateriel+"\nDescription : "+description+"\n");
+                System.out.println("----------------------------------------------------------\n");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void putAgent(String nom, String prenom, String matricule){
+    public static void putMateriel(String nom, LocalDate date_ajout, Date dern_retrait, String description){
 
         try {
 
@@ -42,13 +46,14 @@ public class Agents {
             /* Création de l'objet gérant les requêtes */
             Statement statement = connection.createStatement();
 
-            String INSERT_QUERY = "INSERT INTO agents (nom, prenom, matricule) VALUES (?,?,?)";
+            String INSERT_QUERY = "INSERT INTO materiel (nom, date_ajout, dern_retrait, description) VALUES (?,?,?,?)";
 
             /* Exécution d'une requête d'écriture */
             PreparedStatement st = connection.prepareStatement( INSERT_QUERY, Statement.RETURN_GENERATED_KEYS );
             st.setString( 1, nom );
-            st.setString( 2, prenom );
-            st.setString( 3, matricule );
+            st.setString( 2, String.valueOf(date_ajout));
+            st.setString( 3, String.valueOf(dern_retrait));
+            st.setString(4, description);
             st.executeUpdate();
             ResultSet rs = st.getGeneratedKeys();
 
@@ -58,7 +63,7 @@ public class Agents {
 
     }
 
-    public static void removeAgent(String agentName){
+    public static void removeMateriel(String materielName){
 
         try {
 
@@ -67,7 +72,7 @@ public class Agents {
             /* Création de l'objet gérant les requêtes */
             Statement statement = connection.createStatement();
 
-            String DELETE_QUERY = "DELETE FROM agents WHERE nom = '"+agentName+"'";
+            String DELETE_QUERY = "DELETE FROM materiel WHERE nom = '"+materielName+"'";
 
             /* Exécution d'une requête de suppression */
             PreparedStatement st = connection.prepareStatement( DELETE_QUERY, Statement.RETURN_GENERATED_KEYS );
